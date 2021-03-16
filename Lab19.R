be0cb1c30b7fe61a4fcfa93059b5eeafddedf8b5
HaNeul Jeong <haneulj0510@gmail.com>
2021-03-16 17:37:23 +0900

    3/15 R 실습 수정

--- Lab19.R
+++ Lab19.R
@@ -1,7 +1,7 @@
 # picher_stats_2017.csv 파일을 읽고 원하는 선수에 대한 다음과 같이 삼진, 볼넷, 홈런 성적에 대한 레이더차트를 그려 본다.
 # (다른 성적을 추가해도 됨, 칼라는 임의로)
-
-
+library(dplyr)
+library(fmsb)
 
 ps <- read.csv('data/picher_stats_2017.csv', encoding = "UTF-8")
 str(ps)
@@ -15,7 +15,7 @@ ps2 <- ps1[3,]
 ps3 <- rbind(rep(10,1.5), rep(0,1.5), ps2)
 
 radarchart(ps3,                           # 데이터프레임
-           pcol='red',            # 다각형 선의 색 
+           pcol='red',                   # 다각형 선의 색 
            pfcol=rgb(0.2,0.5,0.5,0.5),   # 다각형 내부 색
            plwd=3,                       # 다각형 선의 두께
            cglcol='grey',                # 거미줄의 색
@@ -26,5 +26,6 @@ radarchart(ps3,                           # 데이터프레임
            axislabcol='grey',            # 축의 레이블 색
            title = "양현종 선수의 성적"
 )
+
 dev.copy(png, "output/Lab19.png")
 dev.off()
