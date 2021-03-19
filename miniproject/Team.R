9e7bc18e236800aac962b109737f5234d7fc2d94
HaNeul Jeong <haneulj0510@gmail.com>
2021-03-19 17:55:54 +0900

    3/19 진행

--- miniproject/Team.R
+++ miniproject/Team.R
@@ -3,101 +3,25 @@ library(XML)
 library(httr)
 library(rvest)
 library(RSelenium)
+
+library(wordcloud)
+library(wordcloud2)
+library(KoNLP)
+library(dplyr)
+library(tm)
+library(htmlwidgets)
+library(showtext)
+showtext_auto()
+
+windowsFonts(lett=windowsFont("휴먼옛체")) 
+windowsFonts(dog=windowsFont("THE개이득"))
+
 # selenium 구동하기
 # java -Dwebdriver.chrome.driver="chromedriver.exe" -jar selenium-server-standalone-4.0.0-alpha-1.jar -port 4445
 
 
-remDr <- remoteDriver(remoteServerAddr = 'localhost', port = 4445, browserName ='chrome')
-remDr$open() 
-remDr$navigate("http://www.google.com")
-
-remDr$navigate("https://www.saramin.co.kr/zf_user/help/live?listType=news&category=10&menu=1&page=")
-
 
 # 취업뉴스
-
-title <- NULL; date <- NULL
-
-for(n in 1:13){
-  site <- paste0('https://www.saramin.co.kr/zf_user/help/live?listType=news&category=10&menu=1&page=',n)
-  text <- read_html(site, encoding = "utf-8")
-  remDr$navigate(text)
-  
-  for(idx in 1:20){
-    jobt <- html_text(html_nodes(text,xpath=paste0('//*[@id="content"]/div[2]/div[2]/table/tbody/tr[',idx,']/td[1]/a')))
-    title[idx] <- jobt
-    
-    jobd <- html_text(html_nodes(text,xpath=paste0('//*[@id="content"]/div[2]/div[2]/table/tbody/tr[',idx,']/td[2]')))
-    date[idx] <- jobd                                     
-  }
-  nextPage <- remDr$findElement(using='css selector', " #content > div.wrap_board > div.pagination.pagination2 > a.btnNext")
-  nextPage$clickElement()
-  Sys.sleep(1)
-}
-
-joblist <- data.frame(title, date)
-
-View(joblist)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# case2
-for(n in 1:13){
-  site <- paste0('https://www.saramin.co.kr/zf_user/help/live?listType=news&category=10&menu=1&page=',n)
-  text <- read_html(site, encoding = "utf-8")
-  
-  jobt <- NULL; jobd <- NULL
-  
-  jobt <- xpathSApply(htmlParse(site),'//*[@id="content"]/div/div/table/tbody/tr/td/a', xmlValue)
-  jobt
-  
-  jobd <-  xpathSApply(htmlParse(site),'//*[@id="content"]/div/div/table/tbody/tr/td', xmlValue)
-  jobd                                     
-}
-nextPage <- remDR$findElement(using='css selector', " #content > div.wrap_board > div.pagination.pagination2 > a.btnNext")
-nextPage$clickElement()
-Sys.sleep(1)
-}
-
-joblist <- data.frame(jobt, jobd)
-
-View(joblist)
-
-
-# case 3
-jobt <- paste0('#content > div.wrap_board > div.tblType > table > tbody > tr:nth-child(',i,') > td.content_tit > a')
-jobd <- paste0('#content > div.wrap_board > div.tblType > table > tbody > tr:nth-child(',i,') > td.date')
-
-jobtitle <- remDr$findElements(using = 'css selector', jobt)
-title <- sapply(jobtitle, function(x) {x$getElementText()})
-title <- unlist(title)
-
-jobdate <- remDr$findElements(using = 'css selector', jobd)
-date <- sapply(jobdate, function(x) {x$getElementText()})
-date <- unlist(date)
-}
-
-joblist <- cbind(title, date)
-joblist <- as.data.frame(joblist)
-View(joblist)
-
-
-
-
 
 
 # case 4
@@ -138,54 +62,102 @@ write.table(newslist$ntitle, "data/newstitle.txt")
 
 
 
-
-library(wordcloud)
-library(wordcloud2)
-library(KoNLP)
-library(dplyr)
-library(tm)
-
-windowsFonts(lett=windowsFont("휴먼옛체")) 
-windowsFonts(dog=windowsFont("THE개이득"))
+# 워드클라우드
 
 
-jobnews <- read.table("data/newstitle.txt", sep="")
-head(jobnews)
-content <- gsub("[[:punct:]]","", jobnews)
+jobnews <- read.table("data/newstitle.txt")
+jobnews <- sapply(jobnews, extractNoun,USE.NAMES = F)
+content <- unlist(jobnews)
+head(content)
 
-content <- Filter(function(x){nchar(x)>=2},content)
+content <- Filter(function(x){nchar(x) >= 2},content)
+content <- gsub("[[:punct:]]"," ", content)
+content <- gsub("[[:digit:]]"," ", content)
 
-
-
-
-
-nouns <- extractNoun(content) 
-
-wordcount <- table(unlist(nouns))
-
-df_word <- as.data.frame(wordcount, stringsAsFactors = ctors = F)
+wordcount <- table(content)
+wordcount_head <- head(sort(wordcount, decreasing = T), 100)
 
 
+sort(wordcount, decreasing = T)
 
-wordcloud(content, 
-          random.order = F,   # 빈도수가 높을수록 가운데 사수
-          rot.per = 0.5, scale = c(4, 1), # 제일 큰 애와 작은 애의 비율
-          colors = rainbow(7), family ="dog")
+newstitle <- wordcloud2(data = wordcount_head, shape = 'pentagon')
 
-  
-
-
-
-
-warnings()
+htmltools::save_html(newstitle,"output/newstitle.html")
 
 
 
 
 # 기업뉴스
-company <- "https://www.saramin.co.kr/zf_user/jobarchive/company-news"
+
+# case3
+remDr <- remoteDriver(remoteServerAddr = 'localhost', port = 4445, browserName ='chrome')
+remDr$open() 
+company <- "https://www.saramin.co.kr/zf_user/jobarchive/company-news/index/page/"
+remDr$navigate(company)
+
+ctitle <- NULL; cdate <- NULL
+  for (j in 2:11){
+    for (p in 1:20){
+      companyt <- remDr$findElements(using='css selector', paste0('#content > div.wrap_recently_news > div.news_list > form > table > tbody > tr:nth-child(',p,') > td:nth-child(1) > dl > dt > a:nth-child(1)'))
+      title <- sapply(companyt, function(x) {x$getElementText()})
+      ctitle <- append(ctitle, unlist(title))
+      
+      companyd <- remDr$findElements(using='css selector', paste0('#content > div.wrap_recently_news > div.news_list > form > table > tbody > tr:nth-child(',p,') > td:nth-child(2)'))
+      date<- sapply(companyd, function(x) {x$getElementText()})
+      cdate <- append(cdate, unlist(date))
+    }    
+    nextPage <- remDr$findElement(using='css selector', paste0("#content > div.wrap_recently_news > div.news_list > form > div.pagination > a:nth-child(",j,")"))
+    nextPage$clickElement()
+    Sys.sleep(1)
+  }
+ 
+ repeat{
+   for (j in 3:12){
+   for (p in 1:20){
+     companyt <- remDr$findElements(using='css selector', paste0('#content > div.wrap_recently_news > div.news_list > form > table > tbody > tr:nth-child(',p,') > td:nth-child(1) > dl > dt > a:nth-child(1)'))
+     title <- sapply(companyt, function(x) {x$getElementText()})
+     ctitle <- append(ctitle, unlist(title))
+     
+     companyd <- remDr$findElements(using='css selector', paste0('#content > div.wrap_recently_news > div.news_list > form > table > tbody > tr:nth-child(',p,') > td:nth-child(2)'))
+     date<- sapply(companyd, function(x) {x$getElementText()})
+     cdate <- append(cdate, unlist(date))
+   }    
+   nextPage <- remDr$findElement(using='css selector', paste0("#content > div.wrap_recently_news > div.news_list > form > div.pagination > a:nth-child(",j,")"))
+   nextPage$clickElement()
+   Sys.sleep(1)
+ }
+}
+
+companynews <- data.frame(title=ctitle[1:3349], date=cdate[1:3349])
+View(companynews)
+write.csv(companynews, "output/companynews.csv")
+
+write.table(companynews$title, "data/companytitle.txt")
 
 
+# 워드클라우드
+
+cpnews <- read.table("data/companytitle.txt")
+cpnews<- sapply(cpnews, extractNoun,USE.NAMES = F)
+cptitle <- unlist(cpnews)
+head(cptitle)
+
+cptitle <- Filter(function(x){nchar(x) >= 2},cptitle)
+cptitle <- gsub("[[:punct:]]"," ", cptitle)
+cptitle <- gsub("[[:digit:]]"," ", cptitle)
+
+cpcount <- table(cptitle)
+cpcount_head <- head(sort(cpcount, decreasing = T), 1000)
 
 
+cpnewstitle <- wordcloud2(data = cpcount_head, shape = 'circle')
 
+htmltools::save_html(cpnewstitle,"output/cpnewstitle.html")
+
+
+# 그래프
+write.table(companynews, "data/cpnews.txt")
+
+(cpnews <- read.table("data/cpnews.txt", header=T))
+as.Date(cpnews$date, format='%Y/%m/%d', sep=".")
+barplot(cpnews$date)
